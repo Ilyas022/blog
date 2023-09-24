5e8d20d8bec325ee5a2b8bd90548bdc7b6275cd7
Ilyas022 <ilya.shchetko@mail.ru>
2023-09-24 13:09:16 +0300

    add api path

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3003/',
+  baseURL: process.env.VITE_API_URL,
 })
 
 axiosInstance.interceptors.request.use((config) => {
