c8da915fb5e495dbd72b3bda1b5b782d3dab3d1a
Ilyas022 <ilya.shchetko@mail.ru>
2023-09-25 14:14:13 +0300

    add fixes2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Layout from './components/Layout'
 import PostPage from './pages/PostPage'
 import Homepage from './pages/HomePage'
@@ -9,27 +9,38 @@ import LogInPage from './pages/LogInPage'
 import AddPostPage from './pages/AddPostPage'
 import UsersPage from './pages/UsersPage'
 import { useActions } from './store/hooks/useActions'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 const App: React.FC = (): JSX.Element => {
   const { getMe } = useActions()
+  const { pathname } = useLocation()
+  const path = pathname.split('/', 2)[1]
+  const title = path ? path[0].toUpperCase() + path.slice(1) : 'Home'
 
   useEffect(() => {
     getMe()
   }, [getMe])
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Homepage />} />
-        <Route path="/posts/:id" element={<PostPage />} />
-        <Route path="/posts/edit/:id" element={<AddPostPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LogInPage />} />
-        <Route path="/user/:id" element={<UserPage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/add-post" element={<AddPostPage />} />
-      </Route>
-    </Routes>
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Homepage />} />
+            <Route path="/posts/:id" element={<PostPage />} />
+            <Route path="/posts/edit/:id" element={<AddPostPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LogInPage />} />
+            <Route path="/user/:id" element={<UserPage />} />
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/add-post" element={<AddPostPage />} />
+          </Route>
+        </Routes>
+      </HelmetProvider>
+    </>
   )
 }
 
